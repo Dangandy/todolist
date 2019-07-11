b2c3683de4d854c4b02bc411fce7d79fbd04b901
Drew <drew@Drews-MacBook.local>
2019-07-11 00:41:39 -0400

    finished skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// 3rd party variables
+import React, { useState } from "react";
 
+// local variables
+
+// React components
 function App() {
+  // variables
+  const [items, setItem] = useState([
+    { name: "1", id: 1, done: false },
+    { name: "2", id: 2, done: false },
+    { name: "3", id: 3, done: false },
+    { name: "4", id: 4, done: false }
+  ]);
+  const [value, setValue] = useState("");
+
+  // when submitted, push target into items
+  const handleSubmit = event => {
+    event.preventDefault();
+    const newItems = [...items, { name: value, id: Date.now(), done: false }];
+    setItem(newItems);
+    setValue("");
+  };
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  const handleDelete = id => {
+    return event => {
+      let newItems = [...items];
+      newItems = newItems.filter(item => {
+        return item.id !== id;
+      });
+      setItem(newItems);
+    };
+  };
+
+  const handleComplete = id => {
+    return event => {
+      let newItems = [...items];
+      newItems = newItems.map(item => {
+        if (item.id === id) {
+          item.done = true;
+        }
+        return item;
+      });
+
+      setItem(newItems);
+    };
+  };
+
+  // return
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          To Do:
+          <input
+            type="text"
+            name="todo"
+            placeholder="Enter your item here!"
+            value={value}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+      <h2>To Do List</h2>
+      <ul>
+        {items.map(item => {
+          let liStyle = {};
+          if (item.done) {
+            liStyle = { "text-decoration": "line-through" };
+          }
+          return (
+            <li key={item.id} style={liStyle}>
+              {" "}
+              {item.name}
+              <button onClick={handleComplete(item.id)}> Done </button>
+              <button onClick={handleDelete(item.id)}> X </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
